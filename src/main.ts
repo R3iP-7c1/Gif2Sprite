c48048c4a93969906da64453d4efd39035804bed
Pierrick <pierrick@pi3r.dev>
2024-07-03 14:34:13 +0200

    [WIP]

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
+import { gifToSprite } from '.';
 import './style.css';
+
+const main = async () => {
+  // const gifUrl = 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTFseGlsdjFwc25rbmtscDE3bTMwaXIxODN1eGpkZ3ExM3IxeGlzciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/g5R9dok94mrIvplmZd/giphy.gif';
+  // const gifUrl = 'https://europe1.discourse-cdn.com/figma/original/3X/7/1/7105e9c010b3d1f0ea893ed5ca3bd58e6cec090e.gif';
+  const gifUrl = 'https://sagapi.sagaxp.fr/uploads/logo_b3e9c4dca6.gif';
+  const data = (await gifToSprite(gifUrl));
+
+  const appElm = document.querySelector('#app')!;
+  const imgElm = document.createElement('img');
+  imgElm.src = data as string;
+
+  appElm.appendChild(imgElm);
+
+}
+
+document.addEventListener('DOMContentLoaded', main);
---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+/** @type {import('vite').UserConfig} */
+export default {
+  resolve: {
+    alias: [{ find: "path", replacement: "path-browserify" }]
+    }
+}
